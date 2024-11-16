5ecd8d435c40b31a38c8f9fa3f0bcf720559d442
purva sharma <sharmapurva5598@gmail.com>
2024-11-16 12:43:02 +0530

    small edit

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ export const Hero = () => {
         <div className={styles.content}>
             <h1 className={styles.title}>Hi, I'm Purva</h1>
             <p className={styles.description}>A final-year Computer Science student at IIIT Kota, I’m passionate about solving complex problems and building impactful solutions. With experience as an Amazon SDE intern and expertise in web development, AWS, and problem solving, I thrive on innovation and collaboration. <br></br><br></br>Explore my work, achievements, and journey—I’m excited to connect and create something amazing together!</p>
-            <a href="mailto:sharmapurva5598@gmail.com" className={styles.contactBtn}>contact me</a>
+            <a href="mailto:sharmapurva5598@gmail.com" className={styles.contactBtn}>Contact me</a>
         </div>
         <img src={getImageUrl("hero/heroImage1.png")} alt="Hero image of me " className={styles.heroImg}/>
         <div className={styles.topBlur}/>
