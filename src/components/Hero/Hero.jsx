58e033e9bb1f24a5f07171e4165e45d5575a72a7
purva sharma <sharmapurva5598@gmail.com>
2024-11-16 12:49:22 +0530

    fixing grammar error

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ export const Hero = () => {
     return <section className={styles.container}>
         <div className={styles.content}>
             <h1 className={styles.title}>Hi, I'm Purva</h1>
-            <p className={styles.description}>A final-year Computer Science student at IIIT Kota, I’m passionate about solving complex problems and building impactful solutions. With experience as an Amazon SDE intern and expertise in web development, AWS, and problem solving, I thrive on innovation and collaboration. <br></br><br></br>Explore my work, achievements, and journey—I’m excited to connect and create something amazing together!</p>
+            <p className={styles.description}>A final-year Computer Science student at IIIT Kota, passionate about solving complex problems and building impactful solutions. With experience as an Amazon SDE intern and expertise in web development, AWS, and problem solving, I thrive on innovation and collaboration. <br></br><br></br>Explore my work, achievements, and journey—I’m excited to connect and create something amazing together!</p>
             <a href="mailto:sharmapurva5598@gmail.com" className={styles.contactBtn}>Contact me</a>
         </div>
         <img src={getImageUrl("hero/heroImage1.png")} alt="Hero image of me " className={styles.heroImg}/>
